d20dbf8b0b9a3353287179813fe6fbcb7c5e1fb6
zhaohc1010 <zhaohc@qdio.ac.cn>
2025-12-14 21:05:36 +0800

    Update config.py

--- intelligent_feeding_tray/config.py
+++ intelligent_feeding_tray/config.py
@@ -8,22 +8,23 @@
 # Vision Model Configuration
 # ARK_API_KEY and Model Endpoint ID
 vision_model_config = {
-  "api_key": "b61c59f0-b48e-4369-8a5d-c63c6b493e52",
-  "model_endpoint": "ep-20251024214904-5pqr9"
+  "api_key": "YOUR_VOLCENGINE_API_KEY",
+  "model_endpoint": "YOUR_ENDPOINT_ID"
 }
 
 # Speech Recognition (ASR) Model Configuration
 # APP_KEY and ACCESS_KEY
 asr_model_config = {
-  "app_key": "8698250401",
-  "access_key": "wNyqv5jKb0FQSSIjE1Yi83Yxp4U-Pw7K"
+  "app_key": "YOUR_API_KEY",
+  "access_key": "YOUR_ACCESS_KEY"
 }
 
 # *** NEW ***
 # Text-to-Speech (TTS) Model Configuration
 # APP_ID, ACCESS_KEY, and RESOURCE_ID
 tts_model_config = {
-  "app_id": "8698250401",
-  "access_key": "wNyqv5jKb0FQSSIjE1Yi83Yxp4U-Pw7K",
-  "resource_id": "volc.service_type.10029"
+  "app_id": "YOUR_APP_ID",
+  "access_key": "YOUR_ACCESS_KEY",
+  "resource_id": "YOUR_RESOURCE_ID"
+
 }
